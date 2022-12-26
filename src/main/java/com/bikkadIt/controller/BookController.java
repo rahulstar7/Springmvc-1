16ff1219b725f206fa8c88168e189c533bacae81
Rahulstar177 <rathodsachinit@gmail.com>
2022-12-26 16:42:54 +0530

    Chanegs by rahul

---
+++ src/main/java/com/bikkadIt/controller/BookController.java
@@ -0,0 +1,44 @@
+package com.bikkadIt.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.bikkadIt.model.Book;
+
+@Controller
+public class BookController {
+
+	@GetMapping("/getBookData")
+	public String getBookData(Model model) {
+		
+		Book bookdata=new Book(111,"Spring",450.00);
+		model.addAttribute("book", bookdata);
+		return "book";
+	}
+	
+	@GetMapping("/books")
+	public ModelAndView getAllBookData() {
+		
+		Book bookdata=new Book(111,"Spring",450.00);
+		Book bookdata1=new Book(222,"Hibernate",550.00);
+		Book bookdata2=new Book(333,"Core Java",600.00);
+		List<Book> listbook=new ArrayList<>();
+		listbook.add(bookdata);
+		listbook.add(bookdata1);
+		listbook.add(bookdata2);
+		
+		ModelAndView m=new ModelAndView();
+		m.addObject("listbook",listbook);
+		m.setViewName("books");
+		
+		return m;
+	}
+	
+	
+	
+}
---
+++ src/main/webapp/views/book.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+  <h2>Book Data</h2>
+  Book Id : ${book.bookId }
+  <br/>BooK Name: ${book.bookName}
+  <br/>Book Price: ${book.bookPrice }
+
+</body>
+</html>
